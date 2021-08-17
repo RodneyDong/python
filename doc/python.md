b924dcce9fe7e245013ab022b18882f692720c3a
RodneyDong <rodneydong156@gmail.com>
2021-08-17 19:45:35 -0400

    turn in homework

--- doc/python.md
+++ doc/python.md
@@ -1,1 +1,5 @@
 # python class notes
+
+
+## python language basics
+[calculate triangle area](../src/function01.py)
---
+++ src/function01.py
@@ -0,0 +1,8 @@
+def triangle_area (b,h):
+    answer = (b*h)/2
+    return answer
+
+if __name__ == "__main__":
+    base = 10
+    height = 15
+    print(triangle_area(base,height))
