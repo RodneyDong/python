7005fda6a842a9ddcf6eacb016075d718cf99ee6
RodneyDong <rodneydong156@gmail.com>
2021-08-31 19:00:13 -0400

    turn in hw

--- hwsolutions/test.py
+++ hwsolutions/test.py
@@ -1,13 +1,3 @@
-y = ['x','yo,']
-x = [',','.']
-y[0] = 'y'
+y = 'XCDOED'
+y = y.replace('X','')
 print(y)
-'''
-for x in words: # Deletes commas or periods
-    for y in words:
-        b = y.replace(y[len(y)-1],'')
-        if x == b:
-            z = words.index(y)
-            x = words[z].replace(words[z][len(y)-1],"")
-            words[z] = x
-'''
